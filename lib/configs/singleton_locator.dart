8534fd5a1e396b098c86634fff59c64d4c0dd617
Simone Colazzo <simone_colazzo>
2024-09-29 00:52:35 +0200

    flavors scaffold

--- lib/configs/singleton_locator.dart
+++ lib/configs/singleton_locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:pokedex_scaffold/configs/flavors.dart';
 
 // ignore: always_use_package_imports
 import './singleton_locator.config.dart';
@@ -12,5 +13,5 @@ GetIt locator = GetIt.instance;
   asExtension: true, // default
 )
 void setupLocator() {
-  locator.init();
+  locator.init(environment: F.locatorEnvironment());
 }
